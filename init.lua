36bf1397ecf8b46bcc3d61e8f22e933f8445378c
Kevin Dillon <kdillo893@gmail.com>
2023-11-08 14:27:58 -0600

    Remove old things, rename if I use again

--- init.lua
+++ init.lua
@@ -154,10 +154,11 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
@@ -241,6 +242,10 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.o.nu = true
+
+-- show relative line numbers
+vim.o.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -252,6 +257,20 @@ vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
+
+-- my indent and tabbing options
+-- 2 spaces per tab 
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+-- tabs to spaces.
+vim.o.expandtab = true
+-- smart indentation depending on code
+vim.o.smartindent = true
+
+-- don't wrap lines ending...
+vim.opt.wrap = false
 
 -- Save undo history
 vim.o.undofile = true
@@ -273,6 +292,25 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- I don't want the highlight for all searched items
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- offset scrolling such that you don't go to the absolute edge
+vim.opt.scrolloff = 8
+-- draw the sign column
+vim.opt.signcolumn = "yes"
+
+-- allow other options for text in filename
+vim.opt.isfname:append("@-@")
+
+-- update swap/edits faster
+vim.opt.updatetime = 50
+
+-- color the column at 80, give an indicator of where I am lengthwise
+vim.opt.colorcolumn = "80"
+
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
@@ -280,14 +318,20 @@ vim.o.termguicolors = true
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- go into Ex for file explore
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
