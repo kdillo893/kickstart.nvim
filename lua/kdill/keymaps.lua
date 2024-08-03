4cd7beb9c70b9586714d6adc1b70f50fa1dfbed5
Kevin Dillon <kdillo893@gmail.com>
2024-08-03 16:04:37 -0500

    adding bnext/bprev

--- lua/kdill/keymaps.lua
+++ lua/kdill/keymaps.lua
@@ -20,3 +20,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- moving around the current visual block
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--for moving between current open buffers fast
+vim.keymap.set("n", "<C-n>", vim.cmd.bnext)
+vim.keymap.set("n", "<C-p>", vim.cmd.bprev)
