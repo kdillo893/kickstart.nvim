7485d65db1be4dc06cb881bf7cb58a38e5a472d0
Kevin Dillon <kdillo893@gmail.com>
2024-11-02 22:14:24 -0500

    some old nvim config inits, forgot to commit

--- init.lua
+++ init.lua
@@ -293,87 +293,30 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    -- A list of parser names, or "all"
+    ensure_installed = { "c", "lua", "javascript", "java" },
 
-    --ignore the following:
-    ignore_install = {},
-
-    --when installing, do synchronous...
+    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+    -- Automatically install missing parsers when entering buffer
     auto_install = false,
 
-    highlight = { 
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "rust" },
+
+    highlight = {
+      -- `false` will disable the whole extension
       enable = true,
-      disable = function(lang, buf) 
-        local max_filesize = 100 * 1024 -- 100KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats.size > max_filesize then
-          return true
-        end
-      end,
 
-      --vim regex highlighting.
-      --additional_vim_regex_highlighting = false,
-    },
-    
-    indent = { enable = true },
+      -- list of language that will be disabled
+      disable = { "c", "rust" },
 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
     },
   }
 end, 0)
@@ -426,20 +369,20 @@ end
 -- document existing key chains
 local wk = require('which-key')
 wk.add({
-  { '<leader>c', group = '[C]ode'},
-  { '<leader>c_', hidden = true},
-  { '<leader>d', group = '[D]ocument'},
-  { '<leader>d_', hidden = true},
-  { '<leader>g', group = '[G]it' },
-  { '<leader>g_', hidden = true},
-  { '<leader>h', group = 'More git' },
-  { '<leader>h_', hidden = true},
-  { '<leader>r', group = '[R]ename' },
-  { '<leader>r_', hidden = true},
-  { '<leader>s', group = '[S]earch' },
-  { '<leader>s_', hidden = true},
-  { '<leader>w', group = '[W]orkspace' },
-  { '<leader>w_', hidden = true},
+  { '<leader>c',  group = '[C]ode' },
+  { '<leader>c_', hidden = true },
+  { '<leader>d',  group = '[D]ocument' },
+  { '<leader>d_', hidden = true },
+  { '<leader>g',  group = '[G]it' },
+  { '<leader>g_', hidden = true },
+  { '<leader>h',  group = 'More git' },
+  { '<leader>h_', hidden = true },
+  { '<leader>r',  group = '[R]ename' },
+  { '<leader>r_', hidden = true },
+  { '<leader>s',  group = '[S]earch' },
+  { '<leader>s_', hidden = true },
+  { '<leader>w',  group = '[W]orkspace' },
+  { '<leader>w_', hidden = true },
 })
 
 -- mason-lspconfig requires that these setup functions are called in this order
