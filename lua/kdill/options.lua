402cac8a5c4da15fcbb01d54779255a3b271ec16
Kevin Dillon <kdillo893@gmail.com>
2025-10-29 16:29:24 -0500

    i'm sick of relnum

--- lua/kdill/options.lua
+++ lua/kdill/options.lua
@@ -9,7 +9,7 @@ vim.o.nu = true
 
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.o.relativenumber = true
+vim.o.relativenumber = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
